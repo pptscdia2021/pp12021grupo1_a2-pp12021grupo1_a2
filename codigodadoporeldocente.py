15579b404f43a593fbb6755a3c84e02ad9403255
Alejandra <89421482+ale22toni@users.noreply.github.com>
2021-09-09 20:38:39 -0300

    código de prueba dado por el docente
    se confirma que funciona correctamente pero puede requerir instalar las siguientes librerías:
    pip install requests
    pip install beautifulsoup
    pip install lxml

---
+++ codigodadoporeldocente.py
@@ -0,0 +1,32 @@
+# import libraries
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+# indicar la ruta
+url_page = 'http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx'
+# tarda 480 milisegundos
+page = requests.get(url_page).text 
+soup = BeautifulSoup(page, "lxml")
+# Obtenemos la tabla por un ID específico
+tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblÍndices'})
+tabla
+name=""
+price=""
+nroFila=0
+for fila in tabla.find_all("tr"):
+    #for row in  tabla.find_all("td")::
+    nroCelda=0
+    for celda in fila.find_all('td'):
+        if nroCelda==0:
+            name=celda.text
+            print("Indice:", name)
+        if nroCelda==2:
+            price=celda.text
+            print("Valor:", price)
+        nroCelda=nroCelda+1
+    nroFila=nroFila+1
+    # Abrimos el csv con append para que pueda agregar contenidos al final del archivo
+    with open('bolsa_ibex35.csv', 'a') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([name, price, datetime.now()])

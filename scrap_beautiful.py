3989567fa877fed059a2a9289c5e5155ec0ed25c
vmsaravia <vanesasaravia8@gmail.com>
2021-09-11 16:21:22 -0300

    commit 1

--- scrap_beautiful.py
+++ scrap_beautiful.py
@@ -4,15 +4,17 @@ from bs4 import BeautifulSoup
 import csv
 from datetime import datetime
 # indicar la ruta
-url_page = 'http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx'
+url_page = 'https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000'
 # tarda 480 milisegundos
 page = requests.get(url_page).text 
 soup = BeautifulSoup(page, "lxml")
 # Obtenemos la tabla por un ID específico
-tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblÍndices'})
+tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblAcciones'})
 tabla
 name=""
 price=""
+cambio=""
+volumen_num=""
 nroFila=0
 for fila in tabla.find_all("tr"):
     #for row in  tabla.find_all("td")::
@@ -20,13 +22,19 @@ for fila in tabla.find_all("tr"):
     for celda in fila.find_all('td'):
         if nroCelda==0:
             name=celda.text
-            print("Indice:", name)
-        if nroCelda==2:
+            print("Acción: ", name)
+        if nroCelda==1:
             price=celda.text
-            print("Valor:", price)
+            print("Valor: ", price)
+        if nroCelda==2:
+            cambio=celda.text
+            print("Variación %:", cambio)
+        if nroCelda==5:
+            volumen_num=celda.text
+            print("Valumen: ", volumen_num)
         nroCelda=nroCelda+1
     nroFila=nroFila+1
     # Abrimos el csv con append para que pueda agregar contenidos al final del archivo
     with open('bolsa_ibex35.csv', 'a') as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow([name, price, datetime.now()])
+        writer.writerow([name, price, cambio, volumen_num, datetime.now()])
